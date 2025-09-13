197f3bc8a33ae5960c732a5efadad5c54e5a69c3
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-13 09:34:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Remove the main element
+document.querySelector('main#main').remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Set id to victory
+newHeader.id = 'victory';
+
+// Add the text
+newHeader.textContent = "Wise is the champion";
